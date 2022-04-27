ff88cfd2d7cbff6b069fd3746f17e50a0cb8e87d
bofmar <bofmar@outlook.com>
2022-04-27 18:03:29 +0300

    Add addBookToLibrary function
    This function takes a book object and pushes it into the Library array

--- script.js
+++ script.js
@@ -21,6 +21,13 @@ Book.prototype.isRead = function(){
   return this.read;
 }
 
+// Library Functions
+const Library = [];
+
+function addBookToLibrary(book){
+  Library.push(book)
+}
+
 // Tests
 
 const b1 = Object.create(Book.prototype);
@@ -28,3 +35,8 @@ b1.init("Test","McTestson",true);
 
 const b2 = Object.create(Book.prototype);
 b2.init("Bob","McBobson",false)
+
+addBookToLibrary(b1);
+addBookToLibrary(b2);
+
+console.table(Library);
