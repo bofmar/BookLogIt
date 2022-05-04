6230246ad06ea50e25d6ac3debde2819e402fae1
bofmar <bofmar@outlook.com>
2022-05-04 14:21:49 +0300

    Refactor the entire script to follow the module pattern

--- script.js
+++ script.js
@@ -1,127 +1,124 @@
-// Selectors
-const section = document.querySelector("section");
-const modal = document.querySelector(".modal");
-const form = document.querySelector("form");
-const openButton = document.querySelector(".open-modal");
-const closeButton = document.querySelector("#close");
-const submitButton = document.querySelector(".submit");
+const library = (function(){
+  const Library = [];
+  
+  //DOM Selectors
+  const section = document.querySelector("section");
+  const modal = document.querySelector(".modal");
+  const form = document.querySelector("form");
+  const openModalButton = document.querySelector(".open-modal");
+  const closeModalButton = document.querySelector("#close");
+  const submitModalButton = document.querySelector(".submit");
 
-// Book prototype
+  // Bind Events
+  section.addEventListener("click", _removeBook);
+  section.addEventListener("click", _changeReadState);
+  openModalButton.addEventListener("click", _openModal);
+  closeModalButton.addEventListener("click", _closeModal);
+  submitModalButton.addEventListener("click", _submitBook);
 
-function Book(){  
-}
+  // Methods
+  function addBookToLibrary(book){
+    Library.push(book);
+    _display();
+  }
 
-Book.prototype.init = function(title,author,read){
+  function _display(){
+    section.textContent = "";
+    section.innerHTML = Library.map((book,i) =>{
+      return `
+        <div class="card" data-attribute=${i}>
+          <h2>"${book.title}"</h2>
+          <h3>${book.author}</h3>
+          <button class="${book.read? "active" : "inactive"}">${book.read ? "Read" : "Not read"}</button>
+          <button class="remove-button">Remove</button>
+        </div>    `
+    }).join("");
+  
+    localStorage.setItem("books", JSON.stringify(Library));
+  }
+
+  function _removeBook(e){
+    if(e.target.className !== "remove-button"){
+      return;
+    }
+    const index = e.target.parentNode.dataset.attribute;
+    Library.splice(index,1);
+    _display();
+  }
+
+  function _changeReadState(e){
+    if(e.target.className !== "active" && e.target.className !== "inactive"){
+      return;
+    }
+    const index = e.target.parentNode.dataset.attribute;
+    Library[index].flipRead();
+    if(Library[index].read){
+      e.target.classList.remove("inactive");
+      e.target.classList.add("active");
+      e.target.innerText = "Read"
+    }
+    else{
+      e.target.classList.remove("active");
+      e.target.classList.add("inactive");
+      e.target.innerText = "Not read";
+    }
+  }
+
+  // function _flipRead(book){
+  //   book.read = !book.read;
+  // }
+
+  function _openModal(){
+    modal.showModal();
+  }
+
+  function _closeModal(){
+    form.reset();
+    modal.close();
+  }
+
+  function _addNewBook(book){
+    const newBook = new Book(book.title, book.author,book.read)
+    addBookToLibrary(newBook);
+  }
+
+  function _submitBook(){
+    const newBook = {
+      title : form.querySelector("#title").value,
+      author : form.querySelector("#author").value,
+      read : form.querySelector("#read").checked
+    };
+  
+    if(newBook.title === "" || newBook.author === ""){
+      return
+    }
+
+    _addNewBook(newBook);
+    _display();
+    _closeModal();
+  }
+
+  // Initialize
+  (function _initialize(){
+    const tempArr = JSON.parse(localStorage.getItem("books")) || [];
+    if(tempArr === []) return;
+
+    tempArr.map(book => {
+      _addNewBook(book);
+    });
+    
+    _display();
+  })();
+
+  return {addBookToLibrary};
+})();
+
+function Book(title,author,read){
   this.title = title;
   this.author = author;
   this.read = read;
 }
 
-Book.prototype.getTitle = function(){
-  return this.title;
-}
-
-Book.prototype.getAuthor = function(){
-  return this.author;
-}
-
-Book.prototype.isRead = function(){
-  return this.read;
-}
-
 Book.prototype.flipRead = function(){
   this.read = !this.read;
 }
-
-// Library Functions
-const Library = [];
-
-function addBookToLibrary(book){
-  Library.push(book)
-}
-
-function displayLibrary(){
-  section.textContent = "";
-  section.innerHTML = Library.map((book,i) =>{
-    return `
-      <div class="card" data-attribute=${i}>
-        <h2>"${book.getTitle()}"</h2>
-        <h3>${book.getAuthor()}</h3>
-        <button class="${book.isRead()? "active" : "inactive"}">${book.isRead()? "Read" : "Not read"}</button>
-        <button class="remove-button">Remove</button>
-      </div>    `
-  }).join("");
-
-  localStorage.setItem("books", JSON.stringify(Library));
-}
-
-// Events
-function removeItem(e){
-  if(e.target.className !== "remove-button"){
-    return;
-  }
-  const index = e.target.parentNode.dataset.attribute;
-  Library.splice(index,1);
-  displayLibrary();
-}
-
-function changeReadState(e){
-  if(e.target.className !== "active" && e.target.className !== "inactive"){
-    return;
-  }
-  const index = e.target.parentNode.dataset.attribute;
-  Library[index].flipRead();
-  if(Library[index].isRead()){
-    e.target.classList.remove("inactive");
-    e.target.classList.add("active");
-    e.target.innerText = "Read"
-  }
-  else{
-    e.target.classList.remove("active");
-    e.target.classList.add("inactive");
-    e.target.innerText = "Not read";
-  }
-}
-
-section.addEventListener("click", removeItem); 
-section.addEventListener("click", changeReadState);
-// delegate the event to the parent, since the children might not exist on page load
-
-openButton.addEventListener("click", ()=>{
-  modal.showModal();
-});
-
-closeButton.addEventListener("click", ()=>{
-  form.reset();
-  modal.close();
-});
-
-submitButton.addEventListener("click", ()=>{
-  const title = form.querySelector("#title").value;
-  const author =form.querySelector("#author").value;
-  const read = form.querySelector("#read").checked;
-
-  if(title === "" || author === ""){
-    return
-  }
-
-  const newBook = Object.create(Book.prototype);
-  newBook.init(title,author,read);
-  addBookToLibrary(newBook);
-  displayLibrary();
-  form.reset();
-  modal.close();
-});
-
-(function initialState(){
-  const tempArr = JSON.parse(localStorage.getItem("books")) || [];
-  if(tempArr === []) return;
-
-  tempArr.map(book => {
-    const newBook = Object.create(Book.prototype);
-    newBook.init(book.title, book.author, book.read);
-    addBookToLibrary(newBook);
-  })
-
-  displayLibrary();
-})()
